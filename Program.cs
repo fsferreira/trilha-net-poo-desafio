b4bb8ba01001d0fe120aa251d278e213ede261d3
fsferreira <fabio.ferreira.mg@gmail.com>
2024-05-19 13:25:58 -0300

    Entrega desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Nokia 1100", "111111111", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("654321", "Iphone 14", "222222222", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
